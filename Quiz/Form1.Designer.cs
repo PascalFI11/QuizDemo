cf8c74582103e283524456fff1d54b2328e0d56d
HettrichFI11 <alien.vs.predator2@web.de>
2015-05-21 13:18:12 +0200

    StartVersion

---
+++ Quiz/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radio_Antwort1 = new System.Windows.Forms.RadioButton();
+            this.radio_Antwort2 = new System.Windows.Forms.RadioButton();
+            this.radio_Antwort3 = new System.Windows.Forms.RadioButton();
+            this.radio_Antwort4 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataSet1 = new System.Data.DataSet();
+            this.oleDbSelectCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbConnection1 = new System.Data.OleDb.OleDbConnection();
+            this.oleDbInsertCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbUpdateCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbDeleteCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbDataAdapter1 = new System.Data.OleDb.OleDbDataAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Maximum = 60;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(534, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "nächste Frage";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radio_Antwort1
+            // 
+            this.radio_Antwort1.AutoSize = true;
+            this.radio_Antwort1.Location = new System.Drawing.Point(12, 191);
+            this.radio_Antwort1.Name = "radio_Antwort1";
+            this.radio_Antwort1.Size = new System.Drawing.Size(14, 13);
+            this.radio_Antwort1.TabIndex = 2;
+            this.radio_Antwort1.TabStop = true;
+            this.radio_Antwort1.UseVisualStyleBackColor = true;
+            // 
+            // radio_Antwort2
+            // 
+            this.radio_Antwort2.AutoSize = true;
+            this.radio_Antwort2.Location = new System.Drawing.Point(12, 232);
+            this.radio_Antwort2.Name = "radio_Antwort2";
+            this.radio_Antwort2.Size = new System.Drawing.Size(14, 13);
+            this.radio_Antwort2.TabIndex = 3;
+            this.radio_Antwort2.TabStop = true;
+            this.radio_Antwort2.UseVisualStyleBackColor = true;
+            // 
+            // radio_Antwort3
+            // 
+            this.radio_Antwort3.AutoSize = true;
+            this.radio_Antwort3.Location = new System.Drawing.Point(12, 280);
+            this.radio_Antwort3.Name = "radio_Antwort3";
+            this.radio_Antwort3.Size = new System.Drawing.Size(14, 13);
+            this.radio_Antwort3.TabIndex = 4;
+            this.radio_Antwort3.TabStop = true;
+            this.radio_Antwort3.UseVisualStyleBackColor = true;
+            // 
+            // radio_Antwort4
+            // 
+            this.radio_Antwort4.AutoSize = true;
+            this.radio_Antwort4.Location = new System.Drawing.Point(12, 319);
+            this.radio_Antwort4.Name = "radio_Antwort4";
+            this.radio_Antwort4.Size = new System.Drawing.Size(14, 13);
+            this.radio_Antwort4.TabIndex = 5;
+            this.radio_Antwort4.TabStop = true;
+            this.radio_Antwort4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Frage";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(52, 90);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(440, 56);
+            this.listBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(310, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Punktestand";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(392, 201);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(313, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Punktestand speichern";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // oleDbSelectCommand1
+            // 
+            this.oleDbSelectCommand1.CommandText = "SELECT Frage, Antwort1, Antwort2, Antwort3, Antwort4, ID FROM tFrage";
+            this.oleDbSelectCommand1.Connection = this.oleDbConnection1;
+            // 
+            // oleDbConnection1
+            // 
+            this.oleDbConnection1.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\\\\172.16.1.1\\Fi2013$\\HettrichPascal\\" +
+    "Eigene Dateien\\Visual Studio 2013\\Projects\\Quiz\\Quiz\\bin\\Debug\\Quiz.mdb";
+            // 
+            // oleDbInsertCommand1
+            // 
+            this.oleDbInsertCommand1.CommandText = "INSERT INTO `tFrage` (`Frage`, `Antwort1`, `Antwort2`, `Antwort3`, `Antwort4`) VA" +
+    "LUES (?, ?, ?, ?, ?)";
+            this.oleDbInsertCommand1.Connection = this.oleDbConnection1;
+            this.oleDbInsertCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("Frage", System.Data.OleDb.OleDbType.VarWChar, 0, "Frage"),
+            new System.Data.OleDb.OleDbParameter("Antwort1", System.Data.OleDb.OleDbType.VarWChar, 0, "Antwort1"),
+            new System.Data.OleDb.OleDbParameter("Antwort2", System.Data.OleDb.OleDbType.VarWChar, 0, "Antwort2"),
+            new System.Data.OleDb.OleDbParameter("Antwort3", System.Data.OleDb.OleDbType.VarWChar, 0, "Antwort3"),
+            new System.Data.OleDb.OleDbParameter("Antwort4", System.Data.OleDb.OleDbType.VarWChar, 0, "Antwort4")});
+            // 
+            // oleDbUpdateCommand1
+            // 
+            this.oleDbUpdateCommand1.CommandText = resources.GetString("oleDbUpdateCommand1.CommandText");
+            this.oleDbUpdateCommand1.Connection = this.oleDbConnection1;
+            this.oleDbUpdateCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("Frage", System.Data.OleDb.OleDbType.VarWChar, 0, "Frage"),
+            new System.Data.OleDb.OleDbParameter("Antwort1", System.Data.OleDb.OleDbType.VarWChar, 0, "Antwort1"),
+            new System.Data.OleDb.OleDbParameter("Antwort2", System.Data.OleDb.OleDbType.VarWChar, 0, "Antwort2"),
+            new System.Data.OleDb.OleDbParameter("Antwort3", System.Data.OleDb.OleDbType.VarWChar, 0, "Antwort3"),
+            new System.Data.OleDb.OleDbParameter("Antwort4", System.Data.OleDb.OleDbType.VarWChar, 0, "Antwort4"),
+            new System.Data.OleDb.OleDbParameter("IsNull_Frage", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Frage", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Frage", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Frage", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Antwort1", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Antwort1", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Antwort1", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Antwort1", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Antwort2", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Antwort2", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Antwort2", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Antwort2", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Antwort3", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Antwort3", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Antwort3", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Antwort3", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Antwort4", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Antwort4", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Antwort4", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Antwort4", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("Original_ID", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "ID", System.Data.DataRowVersion.Original, null)});
+            // 
+            // oleDbDeleteCommand1
+            // 
+            this.oleDbDeleteCommand1.CommandText = resources.GetString("oleDbDeleteCommand1.CommandText");
+            this.oleDbDeleteCommand1.Connection = this.oleDbConnection1;
+            this.oleDbDeleteCommand1.Parameters.AddRange(new System.Data.OleDb.OleDbParameter[] {
+            new System.Data.OleDb.OleDbParameter("IsNull_Frage", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Frage", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Frage", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Frage", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Antwort1", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Antwort1", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Antwort1", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Antwort1", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Antwort2", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Antwort2", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Antwort2", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Antwort2", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Antwort3", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Antwort3", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Antwort3", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Antwort3", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("IsNull_Antwort4", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, ((byte)(0)), ((byte)(0)), "Antwort4", System.Data.DataRowVersion.Original, true, null),
+            new System.Data.OleDb.OleDbParameter("Original_Antwort4", System.Data.OleDb.OleDbType.VarWChar, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "Antwort4", System.Data.DataRowVersion.Original, null),
+            new System.Data.OleDb.OleDbParameter("Original_ID", System.Data.OleDb.OleDbType.Integer, 0, System.Data.ParameterDirection.Input, false, ((byte)(0)), ((byte)(0)), "ID", System.Data.DataRowVersion.Original, null)});
+            // 
+            // oleDbDataAdapter1
+            // 
+            this.oleDbDataAdapter1.DeleteCommand = this.oleDbDeleteCommand1;
+            this.oleDbDataAdapter1.InsertCommand = this.oleDbInsertCommand1;
+            this.oleDbDataAdapter1.SelectCommand = this.oleDbSelectCommand1;
+            this.oleDbDataAdapter1.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "tFrage", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("Frage", "Frage"),
+                        new System.Data.Common.DataColumnMapping("Antwort1", "Antwort1"),
+                        new System.Data.Common.DataColumnMapping("Antwort2", "Antwort2"),
+                        new System.Data.Common.DataColumnMapping("Antwort3", "Antwort3"),
+                        new System.Data.Common.DataColumnMapping("Antwort4", "Antwort4"),
+                        new System.Data.Common.DataColumnMapping("ID", "ID")})});
+            this.oleDbDataAdapter1.UpdateCommand = this.oleDbUpdateCommand1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 350);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radio_Antwort4);
+            this.Controls.Add(this.radio_Antwort3);
+            this.Controls.Add(this.radio_Antwort2);
+            this.Controls.Add(this.radio_Antwort1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radio_Antwort1;
+        private System.Windows.Forms.RadioButton radio_Antwort2;
+        private System.Windows.Forms.RadioButton radio_Antwort3;
+        private System.Windows.Forms.RadioButton radio_Antwort4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Data.DataSet dataSet1;
+        private System.Data.OleDb.OleDbCommand oleDbSelectCommand1;
+        private System.Data.OleDb.OleDbConnection oleDbConnection1;
+        private System.Data.OleDb.OleDbCommand oleDbInsertCommand1;
+        private System.Data.OleDb.OleDbCommand oleDbUpdateCommand1;
+        private System.Data.OleDb.OleDbCommand oleDbDeleteCommand1;
+        private System.Data.OleDb.OleDbDataAdapter oleDbDataAdapter1;
+    }
+}
+
---
+++ Quiz/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.datenbankZugriff();
+        }
+
+        public void datenbankZugriff()
+        {
+            try
+            {
+                this.dataSet1.Tables.Remove("tFrage");
+                oleDbDataAdapter1.Fill(this.dataSet1);
+            }
+            catch (Exception)
+            {
+                oleDbDataAdapter1.Fill(this.dataSet1);
+            }
+
+            frageFüllen();
+        }
+
+        private void frageFüllen()
+        {
+            DataRow r = this.dataSet1.Tables["tFrage"].Rows[0];
+            listBox1.Items.Add(r["Frage"].ToString());
+            radio_Antwort1.Text = r["Antwort1"].ToString();
+            radio_Antwort2.Text = r["Antwort2"].ToString();
+            radio_Antwort3.Text = r["Antwort3"].ToString();
+            radio_Antwort4.Text = r["Antwort4"].ToString();
+            i++;
+            zeit_ablauf();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void zeit_ablauf()
+        {
+            do
+            {
+                System.Threading.Thread.Sleep(10000);
+                progressBar1.PerformStep();
+            } while (progressBar1.Value != progressBar1.Minimum);
+            if (progressBar1.Value == progressBar1.Minimum)
+                test();
+
+        }
+
+        private void test()
+        {
+            if (!radio_Antwort1.Checked && !radio_Antwort2.Checked && !radio_Antwort3.Checked && !radio_Antwort4.Checked)
+            {
+                
+            }
+            if (radio_Antwort1.Checked)
+            {
+                int score = Convert.ToInt32(textBox1.Text);
+                score += 1;
+                textBox1.Text = score.ToString();
+            }
+
+            button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            DataRow r = this.dataSet1.Tables["tFrage"].Rows[i];
+            listBox1.Items.Add(r["Frage"].ToString());
+            radio_Antwort1.Text = r["Antwort1"].ToString();
+            radio_Antwort2.Text = r["Antwort2"].ToString();
+            radio_Antwort3.Text = r["Antwort3"].ToString();
+            radio_Antwort4.Text = r["Antwort4"].ToString();
+            i++;
+            zeit_ablauf();
+            button1.Enabled = false;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //bla
+        }
+
+    }
+}
